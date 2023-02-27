7468e87b9ca9178efab9baf4f694f344a0737bfb
aphexgil <gil.young.1994@gmail.com>
2023-02-27 14:31:14 -0800

    Fixed errors in bio.

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -10,13 +10,14 @@ function AboutMe() {
                 <div className="headshot">
                     <img src={headshot} className="headshot-img"></img>
                 </div>
-                <div className="bio"> I am a creative, curious web developer who is eager to bring his years of classroom
-                    experience to bear on an agile engineering team. My thorough understanding of full-stack
-                    programming and my outstanding quantitative reasoning skills make me an asset in myriad 
-                    devlopment contexts. I am just as comfortable building and styling front-end React 
-                    components as I am debugging Express routes and designing database structures.
-                    Collaberation is the core of everything I do, and I think it is essential that technical
-                    information is communicated clearly, concisely and with kindness. 
+                <div className="bio"> I am a creative, curious web developer who is eager
+                to bring his years of classroom experience to bear on an agile engineering team.
+                My thorough understanding of full-stack programming and my outstanding quantitative
+                reasoning skills make me an asset in myriad development contexts. I am just as
+                comfortable building and styling front-end React components as I am debugging Express
+                routes and designing database structures. Collabaration is the core of everything I do,
+                and I think it is essential that technical information is communicated clearly, concisely
+                and with kindness. 
                 </div>
             </Container>
         </>
