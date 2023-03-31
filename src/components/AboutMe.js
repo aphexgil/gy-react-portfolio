6804efbbb77dc47bf0a1736afeb98f8db5ad2f72
aphexgil <gil.young.1994@gmail.com>
2023-03-31 00:19:12 -0700

    Fixed hooks on Portfolio.js.

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -9,12 +9,12 @@ function AboutMe({aboutSeen, setAboutSeen}) {
     return (
         <>
             <h1 className={aboutSeen ?  "about-title"  : "about-title slide-in-from-top"} onAnimationEnd={()=>setAboutSeen(true)} >About Me</h1>
-            <div  className="main-container">
+            <div className="main-container">
                 <div className="headshot">
                     <img src={headshot} className="headshot-img slide-in-from-left" style={loaded ? {} : { display: 'none' }} onLoad={() => setLoaded(true)}></img>
                 </div>
-                <div className="bio slide-in-from-right"> Hello! My name is Gil Young. I am a creative, curious web developer who is eager to bring his years of classroom experience to bear on an agile engineering team. My thorough understanding of full-stack programming and my outstanding quantitative reasoning skills make me an asset in myriad development contexts. I am just as comfortable building and styling front-end React components as I am designing database structures. Collaboration is at the core of everything I do, and I think it is essential that technical information is communicated clearly, concisely and with kindness. Finally, I am a relentless problem-solver and a natural autodidact––finding novel solutions is my happy place. If you have problems that need solving, don’t hesitate to reach out!
-
+                <div className="bio slide-in-from-right" style={loaded ? {} : { display: 'none' }}>
+                    Hello! My name is Gil Young. I am a creative, curious web developer who is eager to bring his years of classroom experience to bear on an agile engineering team. My thorough understanding of full-stack programming and my outstanding quantitative reasoning skills make me an asset in myriad development contexts. I am just as comfortable building and styling front-end React components as I am designing database structures. Collaboration is at the core of everything I do, and I think it is essential that technical information is communicated clearly, concisely and with kindness. Finally, I am a relentless problem-solver and a natural autodidact––finding novel solutions is my happy place. If you have problems that need solving, don’t hesitate to reach out!
                 </div>
             </div>
         </>
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -15,48 +15,69 @@ import github from '../assets/github.png';
 
 function Portfolio({portfolioSeen, setPortfolioSeen}) {
 
+
+    const [loaded1, setLoaded1] = useState(false);
+    const [loaded2, setLoaded2] = useState(false);
+
+    const [loaded3, setLoaded3] = useState(false);
+
+    const [loaded4, setLoaded4] = useState(false);
+
+    const [loaded5, setLoaded5] = useState(false);
+
+    const [loaded6, setLoaded6] = useState(false);
+
+
     const data = [
         {
             title: 'DBUG',
             link: 'https://cswag-dbug.herokuapp.com/',
             image: dbug,
-            repo: 'https://github.com/saramangelo/cswag-p3'
+            repo: 'https://github.com/saramangelo/cswag-p3',
+            loaded: loaded1,
+            setLoaded: setLoaded1
         },
         {
             title: 'Fantasy Basketball',
             link: 'https://fantasy-league.herokuapp.com/',
             image: fantasybball,
-            repo: 'https://github.com/aphexgil/fantasy-league'
+            repo: 'https://github.com/aphexgil/fantasy-league',
+            loaded: loaded2,
+            setLoaded: setLoaded2
         },
         {
             title: 'FlexFly',
             link: 'https://yacovkopel.github.io/FlexFly/',
             image: flexfly,
-            repo: 'https://github.com/YacovKopel/FlexFly'
+            repo: 'https://github.com/YacovKopel/FlexFly',
+            loaded: loaded3,
+            setLoaded: setLoaded3
         },
         {
             title: 'Tech Blog',
             link: 'https://gy-tech-blog.herokuapp.com/',
             image: techblog,
-            repo: 'https://github.com/aphexgil/tech-blog'
+            repo: 'https://github.com/aphexgil/tech-blog',
+            loaded: loaded4,
+            setLoaded: setLoaded4
         },
         {
             title: 'Code Quiz',
             link: 'https://aphexgil.github.io/code-quiz/',
             image: codequiz,
-            repo: 'https://github.com/aphexgil/code-quiz'
+            repo: 'https://github.com/aphexgil/code-quiz',
+            loaded: loaded5,
+            setLoaded: setLoaded5
         },
         {
             title: 'Weather Dashboard',
             link: 'https://aphexgil.github.io/weather-forecast/',
             image: weatherdash,
-            repo: 'https://github.com/aphexgil/weather-forecast'
+            repo: 'https://github.com/aphexgil/weather-forecast',
+            loaded: loaded6,
+            setLoaded: setLoaded6
         }
     ];
-
-
-
-    const [loaded, setLoaded] = useState(false);
 
     return (
         <>
@@ -65,7 +86,7 @@ function Portfolio({portfolioSeen, setPortfolioSeen}) {
                 <div className="projects-container">
                     {data && data.map((project, i) => (
                         <div id={i} className="project-card" >
-                            <Image src={project.image} className="project-image slide-in-from-top"  style={loaded ? {} : { display: 'none' }} onLoad={() => setLoaded(true)}></Image>
+                            <Image src={project.image} className="project-image slide-in-from-top"  style={project.loaded ? {} : { display: 'none' }} onLoad={() => project.setLoaded(true)}></Image>
                             <div className="overlay">
                                 <div className="overlay-text">
                                     <a href={project.link} target="_blank" className="project-title">{project.title}</a>
